3ac80d0d3fd588eb6e1d20ae8d8a7a3803a20641
saurabh <saurabhayaduvanshi@gmail.com>
2024-08-13 13:36:00 +0530

    first commit for the   initial  stage of react app

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading =React.createElement(
+  "h1",
+  {id:"heading",xyz:"abc"},
+  "Hellow world from React"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);

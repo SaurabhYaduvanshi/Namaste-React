e26fab734f83029e38829e08e589a2f1abccfb11
saurabh <saurabhayaduvanshi@gmail.com>
2024-08-13 17:13:40 +0530

    second commit

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
 const heading =React.createElement(
   "h1",
   {id:"heading",xyz:"abc"},
-  "Hellow world from React"
+  "Hellow world from React and creating new app"
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
